d134e664412748100d3d086fe2b7f56e23e36d3b
Matheus Campos <devcodecampos@gmail.com>
2023-08-06 21:08:32 -0300

    add exit option in menu

--- modulo-02-intro-programacao/calculator/src/index.js
+++ modulo-02-intro-programacao/calculator/src/index.js
@@ -1,17 +1,38 @@
 import PromptSync from "prompt-sync";
 import { calculator } from "./utils/calculator.mjs";
 
-const prompt = PromptSync()
+const prompt = PromptSync();
 
-console.log(`
+let optionMenu;
+do {
+  console.log(`
 CALCULADORA
 (1) Soma
 (2) Subtração
 (3) Multiplicação
-(4) Divisão`)
+(4) Divisão
+(0) Sair`);
 
-let operationNumber = +prompt('Escolha a operação: ')
-let firstNumber = +prompt('Informe o primeiro valor: ')
-let secondNumber = +prompt('Informe o secundo valor: ')
+  let firstNumber = +prompt("Informe o primeiro valor: ");
+  let secondNumber = +prompt("Informe o secundo valor: ");
+  optionMenu = +prompt("Escolha a opção: ");
 
-console.log('Resultado: ' + calculator(firstNumber, secondNumber, operationNumber))
+  switch (optionMenu) {
+    case 1:
+      console.log("Resultado: " + calculator(firstNumber, secondNumber, 1));
+      break;
+    case 2:
+      console.log("Resultado: " + calculator(firstNumber, secondNumber, 2));
+      break;
+    case 3:
+      console.log("Resultado: " + calculator(firstNumber, secondNumber, 3));
+      break;
+    case 4:
+      console.log("Resultado: " + calculator(firstNumber, secondNumber, 4));
+      break;
+    case 0:
+      break;
+    default:
+      console.log("Essa opção não existe");
+  }
+} while (optionMenu !== 0);
