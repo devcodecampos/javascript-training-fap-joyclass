359481409030b4a5a6bc832b237199e97e2be19c
Matheus Campos <devcodecampos@gmail.com>
2023-08-12 22:50:57 -0300

    add people-lists exercise

---
+++ modulo-03-algoritmos-estruturas-de-dados/people-lists/src/index.js
@@ -0,0 +1,38 @@
+const peopleNames = [
+  "Matheus",
+  "Carlos",
+  "José",
+  "Roger",
+  "João",
+  "David",
+  "Ronaldo",
+  "Ted",
+  "Messi",
+  "Romario",
+];
+
+const peopleAges = [25, 23, 78, 32, 41, 14, 53, 31, 36, 44];
+
+const peopleFavoriteColors = [
+  "blue",
+  "red",
+  "yellow",
+  "black",
+  "white",
+  "purple",
+  "pink",
+  "orange",
+  "red",
+  "yellow",
+];
+
+console.log(peopleNames);
+console.log(peopleAges);
+console.log(peopleFavoriteColors);
+
+peopleAges[0] = 100;
+peopleFavoriteColors[0] = "pink";
+
+console.log(peopleNames);
+console.log(peopleAges);
+console.log(peopleFavoriteColors);
